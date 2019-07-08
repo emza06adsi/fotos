117dc929cc09bdc2af27a8b1f01422cf31fcdfba
esteban mza <emeza06@misena.edu.co>
2019-07-07 23:25:08 -0500

    este es el segundo tutorial que esta por corregir

--- src/App.js
+++ src/App.js
@@ -1,29 +1,72 @@
 // import React from 'react';
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
+import firebase from 'firebase'
 class App extends Component {
-render(){
-  return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            < p>
-              Ed  it <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    };
+
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+
+  }
+
+
+
+  componentWillMount() {
+    const db = firebase.firestore()
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user: user });
+    });
+  }
+
+  handleAuth() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado secion`))
+      .catch(error => console.log(`${error.code}: ${error.message}`))
+  }
+
+  handleLogout() {
+    firebase.auth().signOut()
+      .then(result => console.log(`${result.user.email} ha salido `))
+      .catch(error => console.log(`${error.code}: ${error.message}`))
+  }
+
+  renderloginButon() {
+    // si el usuario está logueado
+    if (this.state.user) {
+      return (
+        <div>
+          <img  width="100" src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <p> hola {this.state.user.displayName}!</p>
+          <button onClick={this.handleLogout}> </button>
         </div>
       );
-}
+    } else {
+      return (
+        <button onClick={this.handleAuth}>registrate para ver o ingresar las fotos</button>
+
+      )
+    }
+    // si no haga otra cosa
+
+  }
+
+  render() {
+    return (
+      <div className="App-header">
+        <p>
+          {this.renderloginButon()}
+        </p>
+      </div>
+    );
+  }
 }
 
 // function App() {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -8,7 +9,15 @@ ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
-
+firebase.initializeApp({
+    apiKey: "AIzaSyBF8DUtNH-P8-ncQigA0W9Yp9dOCEhBv1A",
+    authDomain: "fotosfamilia-e2cf5.firebaseapp.com",
+    databaseURL: "https://fotosfamilia-e2cf5.firebaseio.com",
+    projectId: "fotosfamilia-e2cf5",
+    storageBucket: "",
+    messagingSenderId: "848959077894",
+    appId: "1:848959077894:web:fab55f8ac6f6acb2"
+});
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
